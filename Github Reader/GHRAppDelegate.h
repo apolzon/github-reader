dba5961bc833f956f7332b4e1c73b20a6b5b1106
Chris Apolzon <apolzon@gmail.com>
2013-05-19 14:16:14 -0700

    Initial Commit

---
+++ Github Reader/GHRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  GHRAppDelegate.h
+//  Github Reader
+//
+//  Created by Christopher Apolzon on 5/19/13.
+//  Copyright (c) 2013 Apples on the Tree. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GHRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Github Reader/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Github Reader
+//
+//  Created by Christopher Apolzon on 5/19/13.
+//  Copyright (c) 2013 Apples on the Tree. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "GHRAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([GHRAppDelegate class]));
+    }
+}
